c6dabbcc290a224f6f82ffb201056ea8bd89f4b0
jaishilbhavsar <jaishilbhavsar@yahoo.in>
2017-12-25 17:23:37 +0530

    model of login n signup

--- models/user_model.js
+++ models/user_model.js
@@ -1,3 +1,5 @@
+//import { lchmod } from 'fs';
+
 var db = require('../dbconnection');
 
 var User = {
@@ -9,7 +11,14 @@ var User = {
         return db.query("select * from user_tbl where user_id=?", [id], callback);
     },
     addUser: function (u, callback) {
-        return db.query("insert into user_tbl values(?,?,?,?,?,?,?)",[u.user_id,u.user_name,u.user_pass,u.user_pic,u.gender,u.user_mob_no,u.user_bdate],callback);
+        console.log(u.user_id);
+        console.log(u.user_pass);
+        console.log(u.gender);
+        console.log(u.user_name);
+        console.log(u.user_mob_no);
+        console.log(u.user_pic);
+        console.log(u.user_bdate);
+        return db.query("insert into user_tbl(user_id,user_name,user_pass,user_pic,gender,user_mob_no,user_bdate) values(?,?,?,?,?,?,?)",[u.user_id,u.user_name,u.user_pass,u.user_pic,u.gender,u.user_mob_no,u.user_bdate],callback);
     },
     deleteUser: function (id, callback) {
         return db.query("delete from user_tbl where user_id=?", [id],callback);
